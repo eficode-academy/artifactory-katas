31f9d8171370aecfae05197fe8b21fd7623cbac6
Sofus Albertsen <soa@praqma.net>
2018-04-05 16:04:13 +0200

    fixed population method

--- .shared/utils.sh
+++ .shared/utils.sh
@@ -166,29 +166,32 @@ rest_deploy_artifact() {
 download_artifact(){
 
 COUNTER=0
+echo "downloading $ARTIFACTORY_URL/$2 $1 times"
 while [  $COUNTER -lt $1 ]; do
-    curl $ARTIFACTORY_URL/$2 > /dev/null 2>&1
+    curl $ARTIFACTORY_URL$2 > /dev/null 2>&1
     let COUNTER=COUNTER+1 
 done
 }
 
 #Uploads all the files in the three different repos, and makes some fake downloads to simulate usage
 populate_repos(){
+echo "populating repo"
+versions=("1.0.0" "1.4.0" "2.0.1" "3.5.6")
+names=("duck" "fox" "frog" "moose" "squirrel")
+paths=($DUCK_PATH $FOX_PATH $FROG_PATH $MOOSE_PATH $SQUIRREL_PATH)
 
-array=("1.0.0" "1.4.0" "2.0.1" "3.5.6")
-
-for i in "${array[@]}"
+for i in "${versions[@]}"
 do
-    DUCK_URL="/$GRADLE_REPO1/acme/duck/$i/duck-$i.jpg"
-    rest_deploy_artifact "$DUCK_URL" "$DUCK_PATH"
-    if (( RANDOM % 2 )); 
-    then
-        echo "simulating download of artifacts $DUCK_URL" 
-        download_artifact $((RANDOM%10)) "$DUCK_URL" &
-    fi
+    for((o=0;o<${#names[@]};o++))
+    do
+        echo "deploying version $i of ${names[$o]}" &>> $LOGFILE
+        URL="/$GRADLE_REPO1/acme/${names[$o]}/$i/${names[$o]}-$i.jpg"
+        rest_deploy_artifact "$URL" "${paths[$o]}" &>> $LOGFILE
+        if (( RANDOM % 2 )); 
+        then
+            download_artifact $((RANDOM%10)) "$URL" &>> $LOGFILE
+        fi
+    done
 done
-
-DUCK_URL=acme/duck/1.0.0/duck-1.0.0.jpg
-rest_deploy_artifact "/$GRADLE_REPO1/$DUCK_URL" "$DUCK_PATH"
-download_artifact 10 "$GRADLE_REPO1/$DUCK_URL"
+echo "population done"
 }
