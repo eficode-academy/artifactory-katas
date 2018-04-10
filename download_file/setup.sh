397a3855a2c0975610fa25c3ba56c1d1bde5a063
Michael Frikke Madsen <mim@praqma.com>
2018-04-10 10:39:45 +0200

    Delete unused exercise folders

--- download_file/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ../.shared/utils.sh
-
-rest_delete_repository $REPO_NAME
-rest_create_repository $REPO_NAME "generic"
-rest_deploy_artifact "/$REPO_NAME/duck.jpg" "$DUCK_PATH"
--- upload_file/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ../.shared/utils.sh
-initkata
-
-rest_create_repository $GRADLE_REPO1 "generic"
-rest_deploy_artifact "/$GRADLE_REPO1/duck.jpg" "$DUCK_PATH"
