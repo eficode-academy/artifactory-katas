a403eb265e944f74d65e05cab3e47df9c3df7564
Sofus Albertsen <soa@praqma.net>
2018-04-10 11:18:37 +0200

    finalised the setup script #16

--- custom_layouts/setup.sh
+++ custom_layouts/setup.sh
@@ -2,8 +2,13 @@
 
 source ../.shared/utils.sh
 initkata
-# upload duck-1.0.0.jpg and fox-1.0.0.jpg
 
-rest_delete_repository $GRADLE_REPO1
-rest_create_repository $GRADLE_REPO1
+rest_create_repository $GRADLE_REPO1 "gradle"  &>> $LOGFILE
 
+cp $DUCK_PATH ./
+cp $FOX_PATH ./
+
+rest_deploy_artifact "/$GRADLE_REPO1/acme/duck/1.0.0/duck-1.0.0.jpg" "$DUCK_PATH"  &>> $LOGFILE
+rest_deploy_artifact "/$GRADLE_REPO1/acme/fox/1.0.0/fox-1.0.0.jpg" "$FOX_PATH"  &>> $LOGFILE
+
+echo "Setup done. The duck and fox images are in the exercise folder for use in the exercise."
