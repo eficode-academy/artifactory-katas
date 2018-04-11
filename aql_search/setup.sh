1fd0f6ac6d49a61cec5e850c625bafd82832872f
Sofus Albertsen <soa@praqma.net>
2018-04-11 13:48:11 +0200

    Fixed setup script for  #10

--- aql_search/setup.sh
+++ aql_search/setup.sh
@@ -3,13 +3,16 @@
 source ../.shared/utils.sh
 initkata
 
-rest_delete_repository $GRADLE_REPO1
-rest_create_repository $GRADLE_REPO1
-rest_delete_repository $GRADLE_REPO2
-rest_create_repository $GRADLE_REPO2
-rest_delete_repository $GRADLE_REPO3
-rest_create_repository $GRADLE_REPO3
-#DUCK_URL=acme/duck/1.0.0/duck-1.0.0.jpg
-#rest_deploy_artifact "/$GRADLE_REPO1/$DUCK_URL" "$DUCK_PATH"
-#download_artifact 10 "$GRADLE_REPO1/$DUCK_URL"
-populate_repos
+rest_create_repository $MATURITY_1_REPO "gradle" &>> $LOGFILE
+rest_create_repository $MATURITY_2_REPO "gradle" &>> $LOGFILE
+rest_create_repository $MATURITY_3_REPO "gradle" &>> $LOGFILE
+rest_create_repository $MATURITY_4_REPO "gradle" &>> $LOGFILE
+
+
+cp $DUCK_PATH ./
+cp $FOX_PATH ./
+
+
+populate_maturity_repos
+
+echo "Setup done."
