cbd30fbeb146e6a0eb142f2838fe810fe1f6a319
Claus Schneider (Praqma) <claus.schneider@praqma.net>
2018-04-18 12:23:12 +0200

    gh-10 more robust curl download command and clear log.txt at every run

--- aql_search/setup.sh
+++ aql_search/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 source ../.shared/utils.sh
 initkata
 
@@ -9,9 +8,8 @@ rest_create_repository $MATURITY_3_REPO "gradle" &>> $LOGFILE
 rest_create_repository $MATURITY_4_REPO "gradle" &>> $LOGFILE
 
 populate_maturity_repos
-
 arr=( "ARTIFACTORY_URL" "AUTH_HEADER")
 echo_variable_array ${arr[@]}
 echo 'To use your aql, make a json file and use curl like the example below:'
-echo 'curl -i -X POST -H "$AUTH_HEADER"  -H "Content-Type:text/plain" "$ARTIFACTORY_URL"/api/search/aql -T payload.json'
+echo 'curl -i -X POST -H "$AUTH_HEADER"  -H "Content-Type:text/plain" "${ARTIFACTORY_URL}/api/search/aql" -T payload.json'
 echo "Setup done."
