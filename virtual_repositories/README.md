2203d7a5db6df2b3d690439046bdd69073afc6d4
Michael Frikke Madsen <mim@praqma.com>
2018-04-19 20:40:59 +0200

    added gradle setup to virtual_repositories

--- virtual_repositories/README.md
+++ virtual_repositories/README.md
@@ -20,7 +20,6 @@ What we need now is a remote repository and a virtual spanning them all, making 
 * Upload `Duck.jpg` and `Fox.jpg` through the UI, following the repository layout.
 * Add the new repository URL for resolving dependencies to your `build.gradle` file.
 * Add both images to your gradle dependencies in your `build.gradle`.
-* See that they get downloaded with the same dependency declaration no matter what layout artifactory has.
 
-  **Hint:** If you visit your artifact in the Gradle UI, have a look at the `General` tab, under `Dependency Declaration`. Artifactory has already made the dependency declaration for you, ready to paste into your build script.
-- Run `gradle dependencies` and check that your artifact resolves.
+    **Hint:** If you visit your artifact in the Gradle UI, have a look at the `General` tab, under `Dependency Declaration`. Artifactory has already made the dependency declaration for you, ready to paste into your build script.
+* Run `gradle dependencies --refresh-dependencies` and check that your artifact resolves.
--- virtual_repositories/setup.sh
+++ virtual_repositories/setup.sh
@@ -3,16 +3,12 @@
 source ../.shared/utils.sh
 initkata
 
-MATURITY_1_REPO="$KATA_USERNAME-gradle-sandbox-local"
-MATURITY_2_REPO="$KATA_USERNAME-gradle-dev-local"
-MATURITY_3_REPO="$KATA_USERNAME-gradle-v3-local"
-MATURITY_4_REPO="$KATA_USERNAME-gradle-release-local"
+gradle -q init
 
-rest_create_repository $MATURITY_1_REPO "gradle"
-rest_create_repository $MATURITY_2_REPO "gradle"
-rest_create_repository $MATURITY_3_REPO "gradle"
-rest_create_repository $MATURITY_4_REPO "gradle"
-
+rest_create_repository $MATURITY_1_REPO "gradle" &>> $LOGFILE
+rest_create_repository $MATURITY_2_REPO "gradle" &>> $LOGFILE
+rest_create_repository $MATURITY_3_REPO "gradle" &>> $LOGFILE
+rest_create_repository $MATURITY_4_REPO "gradle" &>> $LOGFILE
 
 cp $DUCK_PATH ./
 cp $FOX_PATH ./
@@ -21,5 +17,19 @@ rest_deploy_artifact "/$MATURITY_1_REPO/acme/duck/1.0.0/duck-1.0.0.jpg" "$DUCK_P
 rest_deploy_artifact "/$MATURITY_3_REPO/acme/duck/1.3.0/duck-1.3.0.jpg" "$DUCK_PATH"  &>> $LOGFILE
 rest_deploy_artifact "/$MATURITY_2_REPO/acme/fox/2.3.0/fox-2.3.0.jpg" "$FOX_PATH"  &>> $LOGFILE
 rest_deploy_artifact "/$MATURITY_4_REPO/acme/fox/1.5.3/fox-1.5.3.jpg" "$FOX_PATH"  &>> $LOGFILE
+
+read -d '' CONTENTS << EOF
+repositories {
+    maven { url "$ARTIFACTORY_URL/your_repo_goes_here" }  //Fill in your virtual repository here
+}
+
+configurations { compile }
+
+dependencies {
+    // Add your dependencies here
+}
+
+EOF
+echo "$CONTENTS" >> build.gradle
 
 echo "Setup done. The duck and fox images are in the exercise folder for use in the exercise."
