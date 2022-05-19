0a66b985acce1716ddd5f5568a5eaf374c8f27b3
Sofus Albertsen <sofusalbertsen@gmail.com>
2022-05-19 14:18:28 +0200

    Created parameterized gitlab runner script.

--- setup/start-gilab-runner.sh
+++ setup/start-gilab-runner.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+GITLAB_TOKENS=("GR1348941YxyYzKzr5oK_zc_6KpHX" "GR1348941qXji2Hm5d6CtFxVoBtmk" "GR1348941jCrzkdvZssFn42pRJ7MY" "GR1348941BD329xAFWjKtBjPgT5sy")
+RUNNER_NAME=gitlab-runner
+COUNT=1
+for t in ${GITLAB_TOKENS[@]}; do
+  echo $t
+  echo $COUNT
 
-
-docker run -d --rm --name gitlab-runner \
+docker run -d --rm --name $RUNNER_NAME$t \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v gitlab-runner-config:/etc/gitlab-runner \
+    -v gitlab-runner-config$COUNT:/etc/gitlab-runner \
     gitlab/gitlab-runner:ubuntu
 
-docker exec gitlab-runner gitlab-runner register \
+docker exec $RUNNER_NAME$t gitlab-runner register \
   --non-interactive \
   --executor "docker" \
-  --docker-image alpine:latest \
+  --docker-image ubuntu:latest \
   --url "https://gitlab.com/" \
-  --registration-token "GR13489418qF7Gt-DxxzYkj1FivHf" \
+  --registration-token $t \
+
+((COUNT++))
+done
 
